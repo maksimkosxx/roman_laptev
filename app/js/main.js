0755c9e5d094e91ad15f8c191c4055d114fbed5b
maksimkosxx <ScheglovMN@mos.ru>
2018-03-13 21:41:42 +0300

    add fixed and start mobile adaptation

--- app/js/main.js
+++ app/js/main.js
@@ -1,26 +1,73 @@
 $(document).ready(function () {
 
+
+    // STICKY HEADER
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 10
+        ){
+            $('.wrapper-header').addClass('sticky');
+            $('.sticky').animate({
+                opacity: 1
+            }, 1500);
+        }
+        else{
+            $('.wrapper-header').removeClass('sticky');
+        }
+    });
+
+
+
+    // NAVIGATION SCROLL
+
+    $('.header-nav > li a').on('click', function () {
+
+        $('.header-nav li a').removeClass('active');
+        $(this).addClass('active');
+
+        var scroll_el = $(this).attr('href');
+        if ($(scroll_el).length != 0) {
+            $('html, body').animate({scrollTop: $(scroll_el).offset().top}, 1500);
+        }
+        return false;
+    });
+
+
+    // Scroll to video
+
+    $('.hero-content__link').on('click', function () {
+
+        $('html, body').animate({scrollTop: $('#video').offset().top}, 1500);
+
+        return false;
+    });
+
     // VIDEO youtube
 
-    $(function() {
-        $('.youtube').each(function() {
+    $(function () {
+        $('.youtube').each(function () {
             // По YouTube ID, находим превью картинку
             $(this).css('background-image', 'url(http://i.ytimg.com/vi/' + this.id + '/sddefault.jpg)');
 
             // Добавляем кнопку play
             $(this).append($('<div/>', {'class': 'play'}));
 
-            $('.youtube .play').on('click', function() {
+            $('.youtube .play').on('click', function () {
 
                 $(this).parents().addClass('active');
             });
 
-            $(document).delegate('#'+this.id, 'click', function() {
+            $(document).delegate('#' + this.id, 'click', function () {
                 // Создаем iFrame с параметром autoplay
                 var iframe_url = "https://www.youtube.com/embed/" + this.id + "?autoplay=1&autohide=1";
-                if ($(this).data('params')) iframe_url+='&'+$(this).data('params');
+                if ($(this).data('params')) iframe_url += '&' + $(this).data('params');
 
-                var iframe = $('<iframe/>', {'frameborder': '0', 'src': iframe_url, 'width': $(this).width(), 'height': $(this).height() });
+                var iframe = $('<iframe/>', {
+                    'frameborder': '0',
+                    'src': iframe_url,
+                    'width': $(this).width(),
+                    'height': $(this).height()
+                });
 
                 $(this).replaceWith(iframe);
             });
@@ -30,112 +77,155 @@ $(document).ready(function () {
     // Datepicker
 
 
-    $( function() {
+    $(function () {
         $('#date').datepicker();
-    } );
+    });
 
 
     // Slick
 
-    $('.reviews-slider').slick();
+    $('.reviews-slider').slick({
+        arrows: true,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
 
-//     // Tabs
-//
-//     $('.tabs-nav li').on('click', function () {
-//
-//         var click_id = $(this).attr('id');
-//         var item = '#item_' + click_id;
-//
-//         $('.tabs-nav li').removeClass('active');
-//         $('.tab-content__item').removeClass('active');
-//         $(this).addClass('active');
-//         $(item).addClass('active');
-//     });
-//
-//     // Toggle Examples content
-//
-//     $('.examples-slider__btn').on('click', function (e) {
-//
-//         e.preventDefault();
-//         $(this).toggleClass('active');
-//
-//         if($(this).hasClass('active')) {
-//             $(this).html('Свернуть');
-//             $(this).siblings('.content').slideToggle(350);
-//         } else {
-//             $(this).html('Развернуть');
-//             $(this).siblings('.content').slideUp(350)
-//         }
-//
-//     });
-//
-//     // POPUPS open
-//     function popups() {
-//
-//         var btn = $('span[data-btn]');
-//
-//         btn.on('click',function (e) {
-//             e.preventDefault();
-//
-//             $('.wrapper-popup').fadeIn(400,
-//                 function () {
-//                     $('body').css('overflow', 'hidden');
-//                     $('.popup')
-//                         .css('display', 'block')
-//                         .animate({opacity: 1}, 200);
-//                 });
-//
-//         });
-//
-//         // POPUPS close
-//
-//         $('.popup-close, .wrapper-popup').on('click', function () {
-//             $('body').css('overflow', 'auto');
-//             $('.popup')
-//                 .animate({opacity: 0}, 200,
-//                     function () {
-//                         $(this).css('display', 'none');
-//                         $('.wrapper-popup').fadeOut(400);
-//                     }
-//                 );
-//         });
-//     }
-//
+    // Reviews tabs
+
+    $('.reviews-tabs__button').on('click', function () {
+
+        var click_id = $(this).attr('id');
+        var item = '#item_' + click_id;
+
+        $('.reviews-tabs__button').removeClass('active');
+        $('.reviews-tabs__content').hide('easy').removeClass('active');
+        $(this).addClass('active');
+        $(item).show('easy').addClass('active');
+    });
+
+    // Hide portfolio
+
+    function portfolio() {
+
+        $('.portfolio-item').slice(4).fadeOut();
+
+        $('.portfolio-btn').on('click', function () {
+
+            if ($(this).hasClass('show')) {
+                $('.portfolio-item').slice(4).fadeIn();
+                $(this).removeClass('show').addClass('hide').html('Скрыть');
+                $('html,body').animate({
+                    scrollTop: $(this).offset().bottom
+                }, 1500);
+            } else {
+                $(this).removeClass('hide').addClass('show').html('Смотреть еще');
+                $('.portfolio-item').slice(4).fadeOut();
+
+                $('html, body').animate({scrollTop: $('#portfolio').offset().top}, 1500);
+            }
+        });
+    }
+    portfolio();
+
+
+
+    // POPUPS open
+    function popups() {
+
+        var btn = $('.btn--order');
+
+        btn.on('click', function (e) {
+            e.preventDefault();
+
+            $('.wrapper-popup').fadeIn(400,
+                function () {
+                    $('body').css('overflow', 'hidden');
+                    $('.popup--order')
+                        .css('display', 'block')
+                        .animate({opacity: 1}, 200);
+            });
+        });
+
+        $('.footer-policy').on('click', function (e) {
+            e.preventDefault();
+
+            $('.wrapper-popup').fadeIn(400,
+                function () {
+                    $('body').css('overflow', 'hidden');
+                    $('.popup--policy')
+                        .css('display', 'block')
+                        .animate({opacity: 1}, 200);
+                });
+        });
+
+        // POPUPS close
+
+        $('.popup-close, .btn-close, .wrapper-popup').on('click', function () {
+            $('body').css('overflow', 'auto');
+            $('.popup')
+                .animate({opacity: 0}, 200,
+                    function () {
+                        $(this).css('display', 'none');
+                        $('.wrapper-popup').fadeOut(400);
+                    }
+                );
+        });
+    }
+    popups();
+
 
     // VALIDATION
 
-    // $('form').each(function () {
-    //     $(this).validate({
-    //         errorPlacement: function () {
-    //             return false;
-    //         },
-    //         submitHandler: function (form) {
-    //             $.ajax({
-    //                 type: "POST",
-    //                 url: "/mail.php",
-    //                 data: $(form).serialize(),
-    //                 success: function() {
-    //                     document.location.href="/thanks.php";
-    //                 },
-    //                 error: function() {
-    //                     alert('Данные заполнены некорректно');
-    //                 }
-    //             });
-    //             return false;
-    //         }
-    //     });
-    // });
+    $('form').each(function () {
+        $(this).validate({
+            errorPlacement: function () {
+                return false;
+            },
+            submitHandler: function (form) {
+                $.ajax({
+                    type: "POST",
+                    url: "mail.php",
+                    data: $(form).serialize(),
+                    success: function () {
 
-    // Width Select
+                         var inputName = $('input[name="name"]').val(),
+                             titleText = 'Спасибо, ' + inputName + '!';
 
-    // $('#city').on('change', function() {
-    //
-    //     if($(this).val() == 'spb') {
-    //         $('#city').css('width', '128px');
-    //     } else {
-    //         $('#city').css('width', '60px');
-    //     }
-    // });
+                        if (inputName.length === 0) {
+
+                            $('.popup--thanks .title').html('Спасибо!');
+                        }
+                        else {
+                            $('.popup--thanks .title').html(titleText);
+                        }
+
+                        console.log(inputName);
+
+                        $('.wrapper-popup').fadeIn(400,
+                            function () {
+                                $('body').css('overflow', 'hidden');
+                                $('.popup')
+                                    .css('display', 'none')
+                                    .animate({opacity: 1}, 200);
+                                $('.popup--thanks')
+                                    .css('display', 'block')
+                                    .animate({opacity: 1}, 200);
+                        });
+
+
+                    },
+                    error: function () {
+                        alert('Данные заполнены некорректно');
+                    }
+                });
+                return false;
+            }
+        });
+    });
+
 
     // MOBILE MENU
 
@@ -145,31 +235,6 @@ $(document).ready(function () {
     // });
 
 
-    // ADVANTAGES DESCRIPTION
-
-    // $('.advantages-item__more').on('click', function () {
-    //     $(this).hide();
-    //     $(this).siblings('.advantages-item__description').slideDown();
-    // });
-
-
-    // NAVIGATION SCROLL
-
-    // $('.nav > li a').on('click', function () {
-    //     var scroll_el = $(this).attr('href');
-    //     if ($(scroll_el).length != 0) {
-    //         $('html, body').animate({scrollTop: $(scroll_el).offset().top}, 800);
-    //     }
-    //     return false;
-    // });
-
-    // MENU MOBILE
-
-    // $('.nav-mobile').on('click', function () {
-    //     $(this).toggleClass('active');
-    //     $('.nav-main').slideToggle();
-    // });
-
     // PHONE MASK
 
     // $(function () {
@@ -177,31 +242,4 @@ $(document).ready(function () {
     // });
 
 
-    // VALIDATION
-    // $('form').each(function () {
-    //     $(this).validate({
-    //         errorPlacement: function () {
-    //             return false;
-    //         },
-    //         submitHandler: function (form) {
-    //             $.ajax({
-    //                 type: "POST",
-    //                 url: "/mail.php",
-    //                 data: $(form).serialize(),
-    //                 success: function() {
-    //                     document.location.href="/thanks.php";
-    //                 },
-    //                 error: function() {
-    //                     alert('Данные заполнены некорректно');
-    //                 }
-    //             });
-    //             return false;
-    //         }
-    //     });
-    // });
-
-
-
-
 });
-
--- app/mail.php
+++ app/mail.php
@@ -1,14 +1,11 @@
 <?php
 $to = "maksimkosxx@gmail.com";
-$tema = "Сообщение с сайта Alandr";
+$tema = "Сообщение с сайта ";
 $message = "Ваше имя: " . $_POST['name'] . "<br>";
-$message = "Телефон: " . $_POST['phone'] . "<br>";
+$message = "Телефон: " . $_POST['tel'] . "<br>";
+$message = "Дата мероприятия: " . $_POST['date'] . "<br>";
+$message = "Комментарии: " . $_POST['text'] . "<br>";
 $message = "Почта: " . $_POST['mail'] . "<br>";
-//$message = "Метро / город: " . $_POST['location'] . "<br>";
-//$message = "Сообщение: " . $_POST['message'] . "<br>";
-//$uploaddir = getcwd().DIRECTORY_SEPARATOR.'upload'.DIRECTORY_SEPARATOR;
-//$uploadfile = $uploaddir.basename($_FILES['file']['name']);
-//move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile);
 $headers = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 mail($to, $tema, $message, $headers);
